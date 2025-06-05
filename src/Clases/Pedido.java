750d391caeadd18ca7d67ddc9ebe555de90ca83b
Miguel-FG <miguelfloresgariazo@gmail.com>
2025-06-05 00:06:34 -0500

    actualización de las clases

--- src/Clases/Pedido.java
+++ src/Clases/Pedido.java
@@ -1,15 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Clases;
+package Modelo;
 
-/**
- *
- * @author JOSEPH ROJAS
- */
+import java.util.List;
+
 public class Pedido {
+    private int id;
+    private List<Producto> productos;
+    private String mesa;
+    private double total;
+
+    public Pedido(int id, List<Producto> productos, String mesa, double total) {
+        this.id = id;
+        this.productos = productos;
+        this.mesa = mesa;
+        this.total = total;
+    }
+    // Métodos para agregar, eliminar productos y calcular total
     
-    
+    // Método para calcular el total
+    private double calcularTotal() {
+        double suma = 0;
+        for (Producto producto : productos) {
+            suma += producto.getPrecio();
+        }
+        return suma;
+    }
     
 }
---
+++ src/Clases/Reserva.java
@@ -0,0 +1,12 @@
+
+package Modelo;
+
+import java.time.LocalDateTime;
+
+public class Reserva {
+    private String nombreCliente;
+    private LocalDateTime fechaHora;
+    private int cantidadPersonas;
+
+    // Constructor, getters y setters
+}
