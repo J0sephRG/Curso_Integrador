d24d34eb1ab174c71c9940ae0b8ee7493107ec72
MiguelFloresGariazo <>
2025-07-01 15:40:40 -0500

    Actualizacion de seguridad

---
+++ src/Seguridad/Sesion.java
@@ -0,0 +1,19 @@
+package Seguridad;
+
+import Modelo.Usuario;
+
+public class Sesion {
+    private static Usuario usuarioActual;
+
+    public static Usuario getUsuarioActual() {
+        return usuarioActual;
+    }
+
+    public static void setUsuarioActual(Usuario usuario) {
+        usuarioActual = usuario;
+    }
+
+    public static void cerrarSesion() {
+        usuarioActual = null;
+    }
+}
